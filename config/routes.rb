0cb8582614b3780a0a8f7ba028fc4c0ff5988a6a
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-13 17:22:40 +0300

    Chenge routes to api

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
 
-  post '/auth/developer/callback', to: 'sessions#create'
+  post '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'main#auth_fail'
   get '/logout', to: 'sessions#destroy', as: :logout
 end
--- lib/omniauth/strategies/single_login.rb
+++ lib/omniauth/strategies/single_login.rb
@@ -5,7 +5,13 @@ module OmniAuth
     class SingleLogin < OmniAuth::Strategies::OAuth2
       option :name, :single_login
 
-      option :client_options, { :site => 'http://localhost:3000' }
+      option :client_options, {
+          site: 'http://localhost:3000',
+          # request_token_path: '/api/v1/authorizations/request_token',
+          # access_token_path:  '/api/v1/authorizations/access_token',
+          token_url: '/api/v1/authorizations/request_token',
+          authorize_url: 'http://localhost:3000/api/v1/authorizations/authorize'
+      }
 
       uid{ raw_info['id'] }
 
@@ -23,7 +29,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/me').parsed
+        @raw_info ||= access_token.get('/api/v1/authorizations/me').parsed
       end
     end
   end
